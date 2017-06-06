67841ca795060078f86e66fa882e9ea114689e80
max <max@maauer.com>
2017-06-06 21:21:25 +0000

    begin stuff to insert content in to page files

--- glowingleaves.py
+++ glowingleaves.py
@@ -1,7 +1,11 @@
 import os
 
+basedir = '.'
+pagedir = basedir + '/content/pages'
+postdir = basedir + '/content/posts'
+
 #list all files in directory
-listdir = os.listdir(path='.')
+listdir = os.listdir('.')
 
 #remove .git from list if it exists, if not ignore it.
 try:
@@ -10,3 +14,29 @@ except:
 	pass
 
 print (listdir)
+
+#pages
+listpages = os.listdir(pagedir)
+print (listpages)
+
+# https://stackoverflow.com/a/3961385/7327807
+def get_line_number(phrase, file_name):
+	with open(file_name, 'r') as openfile:
+		for line_i, line in enumerate(openfile, 1):
+			if phrase in line:
+				return line_i
+	openfile.close()
+
+for i in range (0, len(listpages)):
+	page_to_open = pagedir + '/' + listpages[i]
+	
+	lineNum = get_line_number('<style>', page_to_open)
+	print(lineNum)
+	
+	pagefile = open(page_to_open, 'r')
+	pagefile.close()
+
+
+
+
+
