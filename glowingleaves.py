43f73a24048b0528b178d3196b4f1b4329d2c7f8
max <max@maauer.com>
2017-06-06 18:31:12 +0000

    add moer python dir list stuff

--- glowingleaves.py
+++ glowingleaves.py
@@ -1,3 +1,12 @@
 import os
 
-os.listdir(path='.')
+#list all files in directory
+listdir = os.listdir(path='.') 
+
+#remove .git from list if it exists, if not ignore it.
+try:
+	listdir.remove('.git')
+except:
+	pass
+
+print (listdir)
